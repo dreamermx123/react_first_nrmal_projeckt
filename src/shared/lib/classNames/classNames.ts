9adc8649dd9367e651b6320e85757be10466136b
dreamermx123 <dzargo1@yandex.ru>
2025-02-04 22:10:42 +0300

    firs ui kit component

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,10 +1,26 @@
 
 type Mods = Record<string, boolean | string>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
+    // console.log(cls, additional, mods)
+    // console.log([
+    //     cls,
+    //     ...additional.filter(Boolean),
+    //     ...Object.entries(mods)
+    //         .filter(([className, value]) => Boolean(value))
+    //         .map(([className]) => className)
+    // ].join(' '))
+    // console.log( [
+    //     cls,
+    //     ...additional.filter(Boolean),
+    //     ...Object.entries(mods)
+    //         .filter(([className, value]) => Boolean(value))
+    //         .map(([className]) => className)
+    // ]
+    //     .join(' '))
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className]) => className)
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,32 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./AppLink.module.scss"
+import { FC } from "react";
+import { LinkProps, Link } from "react-router-dom";
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY='secondary'
+}
+
+interface AppLinkProps extends LinkProps{
+    className?:string;
+    theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (
+    { className, 
+        children, 
+        to, 
+        theme = AppLinkTheme.PRIMARY, 
+        ...otherProps }: AppLinkProps)=>{
+    return (
+        <Link 
+            to={to} 
+            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+            {...otherProps}
+            >
+           
+            {children}
+        </Link>
+    )
+}
