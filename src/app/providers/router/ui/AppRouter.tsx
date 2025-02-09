ead7f882c33f11c7c65161bfb31711fc113b2964
dreamermx123 <dzargo1@yandex.ru>
2025-02-10 00:19:10 +0300

    14 Sidebar. Layout приложения Метка

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,9 +1,7 @@
-import React from "react"
-import { MainPage } from "pages/MainPage"
 import { Suspense } from "react"
 import { Routes, Route } from "react-router-dom"
 import { routeConfig } from "shared/config/routeConfig/routeConfig"
-import AboutPage from "pages/AboutPage/ui/AboutPage"
+
 const AppRouter = () => {
     return (
         
@@ -17,7 +15,10 @@ const AppRouter = () => {
                     path={path}
                     element={(
                         <Suspense fallback={<div>Loading...</div>}>
-                            {element}
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                            
                         </Suspense>
 
                     )}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -12,7 +12,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.navbar, {}, [])}>
-            <ThemeSwitcher />
+           
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>Главная</AppLink>
                 <AppLink theme={AppLinkTheme.SECONDARY}  to={'/about'} className={cls.mainLink}>О сайте</AppLink>
